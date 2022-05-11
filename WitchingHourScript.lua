d61587c0b224894fb6b40319cb312d9371e3d423
Iumentis <105241460+Iumentis@users.noreply.github.com>
2022-05-11 19:22:50 +0100

    v5
    Fixed Autofarm but you must reset your character to start the AutoFarm after turning it on.
    Added link to my WeAreDevs.net profile
    In v6 or v7 I will try to make a toggle on and off AutoFarm

--- WitchingHourScript.lua
+++ WitchingHourScript.lua
@@ -1,55 +1,34 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/batusz/uilibrarys/main/mercury_ui_lib"))()
+game.Workspace.NailGiver.CFrame = CFrame.new(1149.9324951171875, 61.9999885559082, -814.690673828125)
 
--- Made by Airwav#2162
-game:service'Players'.LocalPlayer.Idled:connect(function()
-bb:CaptureController()bb:ClickButton2(Vector2.new())
+local player = game.Players.LocalPlayer
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/juywvm/ui-libs/main/Flux_Library/FluxLibrary")()
+local win = Flux:Window("Witching Hour", "AutoFarm", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
+local tab = win:Tab("AutoFarm Tab", "http://www.roblox.com/asset/?id=6023426915")
+tab:Button("Get Link to WeAreDevs.Net Profile", "This will copy my WeAreDevs.Net Profile", function()
+Flux:Notification("Succesfully Copied Link", "Thankyou!")
+setclipboard("https://wearedevs.net/profile?uid=55841")
 end)
-
-
-local gui = Library:create{
-    Theme = Library.Themes.Serika
-}
-
-local tab = gui:tab{
-    Icon = "rbxassetid://6034996695",
-    Name = "AutoFarm"
-}
-
-tab:button({
-    Name = "AutoFarm",
-    Callback = function()
-        tab:prompt{
-            Title = "AutoFarm Preparing OwO",
-            Text = "JK",
-            Buttons = {
-                AutoFarm = function()
-                    while true do wait(0.1)
-                        if game:GetService("Players").LocalPlayer.TeamColor ~= game:GetService("Teams").Spectating.TeamColor then
-                        local player = game.Players.LocalPlayer
-                        player.Character.Head:Destroy()
-                    else wait(0.1)
-                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1149.9324951171875, 61.9999885559082, -814.690673828125)    
-                        game.Workspace.NailGiver.CFrame = CFrame.new(1149.9324951171875, 61.9999885559082, -814.690673828125)
-                                        end
-                                    end
-                                end
-                            }
-                        }
-                    end,
-                    })
-tab:button({
-    Name = "AutoFarmOff (Not working)",
-    Callback = function()
-        tab:prompt{
-            Title = "This no longer works. Please wait for update!",
-            Text = "Do You Still Want To Continue. This will respawn your player",
-            Buttons = {
-                        Yes = function()
-                    local player = game.Players.LocalPlayer
-                       player.Character.Head:Destroy()
-                                    end
-                                }
-                            }
-                        end,
-                    })
---Commit Change (Don't mind me)
+tab:Label("Made by DadComeBack at WeAreDevs.Net")
+tab:Label("After clicking AutoFarm you MUST restart your character")
+tab:Line()
+tab:Button("AutoFarm", "This will start the AutoFarm!", function()
+Flux:Notification("Starting", "Thankyou!")
+                    game.Players.LocalPlayer.CharacterAdded:Connect(function() --Help from Kuraise
+                while true do wait(0.1)
+                            task.spawn(function() --Help from Kuraise
+                if  game.Players.LocalPlayer.TeamColor == game:GetService("Teams").Playing.TeamColor then
+                    player.Character.Head:Destroy()
+                elseif game:GetService("Players").LocalPlayer.TeamColor == game:GetService("Teams").Spectating.TeamColor then
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1149.9324951171875, 61.9999885559082, -814.690673828125)
+                        end
+                    end)
+                end
+            end)
+        end)
+tab:Button("Anti-Afk", "This will start the AntiAfk!", function()
+    Flux:Notification("Anti-Afk is On", "Okay!")
+                -- Made by Airwav#2162
+                game:service'Players'.LocalPlayer.Idled:connect(function()
+                bb:CaptureController()bb:ClickButton2(Vector2.new())
+            end)
+        end)
